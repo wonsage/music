3873a6d214f4866cd658ac6a609f88ab30d09d1b
wonSage <34784300+wonsage@users.noreply.github.com>
2023-03-20 10:50:49 +0800

    Demo init.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/base.css'
+import './assets/main.css'
+
+const app = createApp(App)
+
+// 注册插件
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
