bf406e4a172024c1b94e3926a694b4d0af68dfee
wonSage <34784300+wonsage@users.noreply.github.com>
2023-04-03 16:12:23 +0800

    Vue Router Config

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,22 +3,26 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link :to="{ name: 'home' }" exact-active-class="no-active"
+        class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link :to="{ name: 'about' }" class="px-2 text-white">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+              <router-link class="px-2 text-white" href="#" :to="{ name: 'manage' }">Manage</router-link>
             </li>
 
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
             </li>
           </template>
         </ul>
@@ -31,10 +35,11 @@
 import { mapStores } from 'pinia';
 import useModalStore from '@/stores/modal';
 import useUserStore from '@/stores/user';
+import { RouterLink } from 'vue-router';
 
 export default {
   components: {
-
+    RouterLink
   },
   computed: {
     ...mapStores(useModalStore, useUserStore),
@@ -43,7 +48,12 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
       console.log(this.modalStore.isOpen)
-    }
+    },
+    signOut() {
+      this.userStore.signOut();
+      if (this.$route.meta.requiresAuth)
+        this.$router.push({ name: 'home' })
+    },
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HomeView from '@/views/HomeView.vue'
+import HomeAbout from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
+import useUserStore from '@/stores/user'
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    // component 应是@/views下的组件
+    component: HomeView,
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: HomeAbout,
+  },
+  {
+    name: 'manage',
+    alias: '/manage', // avoid redirecting
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+    console.log('boforeEnter Guard')
+      next()
+    },
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    // 其他路径都重定向回首页
+    name: 'error',
+    path: '/:catchAll(.*)*',
+    redirect: {
+      name: 'home',
+    }
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  // 配置导航链接激活时样式
+  linkExactActiveClass: 'text-yellow-500',
 })
 
+// 全局路由守卫
+router.beforeEach((to, from, next) => {
+  // console.log('global beforeRouter')
+  if(!to.meta.requiresAuth) {
+    next()
+  } else if (useUserStore().userLoggedIn) {
+    console.log('已登录')
+    next()
+  } else {
+    console.log('未登录，回首页')
+    next({name: 'home'})
+  }
+})
 export default router
