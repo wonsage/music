826cf4c5b3482ec84b5c23bad6e779661ee9cd5e
wonSage <34784300+wonsage@users.noreply.github.com>
2023-04-23 14:25:52 +0800

    Add publicPath.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+let publicPath = './'
+if (process.env.NODE_ENV === 'production') {
+  publicPath = 'https://wonsage.github.io'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/muzic/',
+  publicPath,
   plugins: [vue()],
   resolve: {
     alias: {
