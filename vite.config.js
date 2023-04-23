ac6aacb44221417f34f21babf612615789821deb
wonSage <34784300+wonsage@users.noreply.github.com>
2023-04-23 14:36:27 +0800

    Add base of Vite.Config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 let publicPath = './'
+let base = '/'
 if (process.env.NODE_ENV === 'production') {
+  base= '/muzic/'
   publicPath = 'https://wonsage.github.io'
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/muzic/',
+  base,
   publicPath,
   plugins: [vue()],
   resolve: {
